f4bafd5aa70a4a4ea18a28f84affbad72879a082
Lucas Silva <lucas.cristiano96@gmail.com>
2025-04-07 14:55:42 -0300

    implemented product form

--- app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
@@ -1,19 +1,17 @@
 package br.com.alura.orgs.ui.activity
 
-import android.app.Activity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import br.com.alura.orgs.R
 import br.com.alura.orgs.model.Produto
 import br.com.alura.orgs.ui.recycleview.adapter.ListaProdutosAdapter
 import java.math.BigDecimal
 
-class MainActivity: Activity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 //        val nome = findViewById<TextView>(R.id.nome)
 //        nome.text = "Cesta de Frutas"
 //        val descricao = findViewById<TextView>(R.id.descricao)
@@ -21,18 +19,20 @@ class MainActivity: Activity() {
 //        val valor = findViewById<TextView>(R.id.valor)
 //        valor.text = "19.99"
         val recycleView = findViewById<RecyclerView>(R.id.recycleView)
-        recycleView.adapter = ListaProdutosAdapter(context = this, produtos = listOf(
-            Produto(
-                nome = "Teste",
-                descricao = "Teste Descrição",
-                valor = BigDecimal("19.99")
-            ),
-            Produto(
-                nome = "Teste 1",
-                descricao = "Teste Descrição 1",
-                valor = BigDecimal("19.99")
-            ),
-        ))
+        recycleView.adapter = ListaProdutosAdapter(
+            context = this, produtos = listOf(
+                Produto(
+                    nome = "Teste",
+                    descricao = "Teste Descrição",
+                    valor = BigDecimal("19.99")
+                ),
+                Produto(
+                    nome = "Teste 1",
+                    descricao = "Teste Descrição 1",
+                    valor = BigDecimal("19.99")
+                ),
+            )
+        )
 //        recycleView.layoutManager = LinearLayoutManager(this)
     }
 }
