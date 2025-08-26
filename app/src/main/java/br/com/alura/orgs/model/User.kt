8932e0040a1ca545ea95bb10ac63c6380dfc2da0
Lucas Silva <lucas.cristiano96@gmail.com>
2025-08-25 21:07:04 -0300

    class user

---
+++ app/src/main/java/br/com/alura/orgs/model/User.kt
@@ -0,0 +1,7 @@
+package br.com.alura.orgs.model
+
+data class User(
+    val id: String,
+    val name: String,
+    val password: String
+)
--- app/src/main/java/br/com/alura/orgs/ui/activity/FormRegisterActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/FormRegisterActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import br.com.alura.orgs.databinding.ActivityFormRegisterBinding
+import br.com.alura.orgs.model.User
 
 class FormRegisterActivity : AppCompatActivity() {
     private val binding by lazy {
@@ -21,7 +22,16 @@ class FormRegisterActivity : AppCompatActivity() {
         val saveButton = binding.activityFormRegisterButton
 
         saveButton.setOnClickListener {
-            Log.i("Register Button", "configRegisterButton: Register")
+            val newUser = createUser()
+            Log.i("CadastrarUsuario", "onCreate: $newUser")
+            finish()
         }
+    }
+
+    private fun createUser(): User {
+        val user = binding.activityFormRegisterUser.text.toString()
+        val name = binding.activityFormRegisterName.text.toString()
+        val password = binding.activityFormRegisterPassword.text.toString()
+        return User(user ,name, password)
     }
 }
